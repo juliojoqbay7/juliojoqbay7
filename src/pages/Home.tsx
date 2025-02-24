a89dfe1f8c5aa9213665abf8ff565aae6e56f809
juliojoqbay7 <motivacaojoqbay@gmail.com>
2025-02-23 22:17:37 -0300

    feat: Create initial pages (Home, About, Projects, Contact

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+
+import Header from "../components/Header";
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>Julio Joqbay</h1>
+        </>
+    );
+}
+
+export default Home;
